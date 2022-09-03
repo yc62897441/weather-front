39efb3bd3b02063f4785fff7fa86aa923609c5e9
yc62897441 <yc62897441@gmail.com>
2022-09-03 15:25:58 +0800

    引入、裝設ol套件(openLayers)

--- src/views/WeatherMap.vue
+++ src/views/WeatherMap.vue
@@ -4,6 +4,7 @@
     <template>
       <div class="main-container main-container-weathermap">
         <h1>天氣地圖</h1>
+        <div id="map"></div>
       </div>
     </template>
     <Footer />
@@ -15,6 +16,11 @@ import Navbar from '../components/Navbar.vue'
 import Footer from '../components/Footer.vue'
 import { mapState } from 'vuex'
 
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import XYZ from 'ol/source/XYZ';
+
 export default {
   components: {
     Navbar,
@@ -23,5 +29,37 @@ export default {
   computed: {
     ...mapState(['datasetOneWeek', 'datasetPerThreeHours'])
   },
+  methods: {
+    initMap() {
+      new Map({
+        target: 'map',
+        layers: [
+          new TileLayer({
+            source: new XYZ({
+              url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+            })
+          })
+        ],
+        view: new View({
+          center: [0, 0],
+          zoom: 2
+        })
+      });
+    }
+  },
+  mounted() {
+    this.initMap()
+  }
 }
 </script>
+
+<style>
+#map {
+  display: block;
+  /* position: absolute;
+  top: 100px;
+  bottom: 0; */
+  width: 600px;
+  height: 400px;
+}
+</style>
