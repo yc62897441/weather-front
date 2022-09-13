ab35fc758d84a34a180622041226564bceb5546b
yc62897441 <yc62897441@gmail.com>
2022-09-13 14:05:08 +0800

    調整router.beforeEach邏輯，新增需驗證路由usersave

--- src/router/index.js
+++ src/router/index.js
@@ -54,34 +54,30 @@ const router = new VueRouter({
 })
 
 router.beforeEach(async (to, from, next) => {
-  // 需驗證使用者的頁面
-  const pathsWithAuth = ['test']
+  // 從 localStorage 取出 token，從 vuex 取出 token
+  // 如果有登入了，兩者都會有東西
+  const token = localStorage.getItem('token')
+  const tokenInStore = store.state.token
+  let currentUser = store.state.currentUser
 
-  if (pathsWithAuth.includes(to.name)) {
-    // 從 localStorage 取出 token，從 vuex 取出 token
-    // 如果有登入了，兩者都會有東西
-    const token = localStorage.getItem('token')
-    const tokenInStore = store.state.token
-    let currentUser = store.state.currentUser
+  // 如果有登入了，store.state.isAuthenticated 會是 true
+  let isAuthenticated = store.state.isAuthenticated
 
-    // 如果有登入了，store.state.isAuthenticated 會是 true
-    let isAuthenticated = store.state.isAuthenticated
+  // 如果 localStorage 有 token，且跟 vuex 的 token 一致時，跳過以下步驟，不須重新檢查  localStorage 的 token 是否還正確
+  // 如果 localStorage 有 token，但 token 跟存在 vuex 的 token 不一致時，要重新檢查 localStorage 的 token 是否還正確
+  // fetchCurrentUser() 會攜帶 localStorage 的 token 向後端 getCurrentUser()，藉此檢查 localStorage 的 token
+  // 如果 localStorage 的 token 還是有效，isAuthenticated = true 否則為 false
+  if (token && (token !== tokenInStore)) {
+    // 取得驗證成功與否 fetchCurrentUser() 回傳 true or false
+    isAuthenticated = await store.dispatch('fetchCurrentUser')
+    currentUser = store.state.currentUser
+  }
 
-    // 如果 localStorage 有 token，且跟 vuex 的 token 一致時，跳過以下步驟，不須重新檢查  localStorage 的 token 是否還正確
-    // 如果 localStorage 有 token，但 token 跟存在 vuex 的 token 不一致時，要重新檢查 localStorage 的 token 是否還正確
-    // fetchCurrentUser() 會攜帶 localStorage 的 token 向後端 getCurrentUser()，藉此檢查 localStorage 的 token
-    // 如果 localStorage 的 token 還是有效，isAuthenticated = true 否則為 false
-    if (token && (token !== tokenInStore)) {
-      // 取得驗證成功與否 fetchCurrentUser() 回傳 true or false
-      isAuthenticated = await store.dispatch('fetchCurrentUser')
-      currentUser = store.state.currentUser
-    }
-
-    // 如果 isAuthenticated 為 false，且進入需要驗證的頁面，則轉址到登入頁
-    if (!isAuthenticated) {
-      next('/signin')
-      return
-    }
+  // 如果 isAuthenticated 為 false，且進入需要驗證的頁面，則轉址到登入頁
+  const pathWithAuth = ['usersave']
+  if (!isAuthenticated && pathWithAuth.includes(to.name)) {
+    next('/signin')
+    return
   }
 
   if (!store.state.isFetchDatasetOneWeek) {
